c3fc266ef40c53c0a8894e82387fde1314d51de2
Eduardo12305 <eumarinho@outlook.com>
2024-10-18 11:29:13 -0300

    Feat: deploy vercel 1.1

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [UserController::class, 'store']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
+Route::delete('/delete/{id}', [UserController::class, 'destroy']);
 Route::post('login', [UserController::class, 'login']);
 
 Route::get('busca', [UserController::class, 'busc']);
