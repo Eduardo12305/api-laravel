74b28ab0fe25d6ca2f65b909cd18b192f771a36a
Eduardo12305 <eumarinho@outlook.com>
2024-10-18 14:35:11 -0300

    Feat: deploy

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,13 @@ Route::get('/', function () {
 });
 
 // Route::post('register', [UserController::class, 'register']);
+
+Route::get('/hello', function () {
+    return response()->json(['message' => 'hello']);
+});
+
+Route::post('register', [UserController::class, 'store']);
+Route::delete('/delete/{id}', [UserController::class, 'destroy']);
+Route::post('login', [UserController::class, 'login']);
+
+Route::get('busca', [UserController::class, 'busc']);
