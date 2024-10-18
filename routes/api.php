100d6e8364da13d24b81ed28cf2d8feb4c00b5b7
Eduardo12305 <eumarinho@outlook.com>
2024-10-18 14:39:04 -0300

    Feat: deply version .

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 Route::post('register', [UserController::class, 'store']);
 Route::delete('/delete/{id}', [UserController::class, 'destroy']);
