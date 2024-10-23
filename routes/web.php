bf1cecf20db8c21223edd1125420e74f83c793a3
Eduardo12305 <eumarinho@outlook.com>
2024-10-23 12:57:53 -0300

    Feat: api.php funcionando

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\PlanoController;
 use Illuminate\Http\Request;
 
 // Route::get('register', [UserController::class, 'register']);
@@ -11,34 +9,3 @@ use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::post('register', [UserController::class, 'register']);
-
-// Route::get('/hello', function () {
-//     return response()->json(['message' => 'hello']);
-// });
-
-Route::post('register', [UserController::class, 'store']);
-
-
-Route::get('/user/view', [UserController::class, 'view']); // para fins de teste
-Route::post('/user/create', [UserController::class, 'create'])->name('user.store'); //para fins de teste
-
-Route::get('/user/login', [UserController::class, 'viewlog']);
-Route::post('/user/log', [UserController::class, 'login'])->name('user.login');
-
-Route::delete('/delete/{id}', [UserController::class, 'destroy']);
-
-Route::post('/login', [UserController::class, 'login']);
-
-Route::get('/busca', [UserController::class, 'busc']);
-
-
-Route::get('/addplano', [PlanoController::class, 'store']); //adicionar os planos
-
-
-Route::get('/planos/all', [PlanoController::class, 'planoAll']);
-
-Route::delete('/end', [PlanoController::class, 'deleteAll']);
-
-
